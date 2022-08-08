5f706893fe1fab03f7a40ec740aebfa241498b06
Fernando Paulino <xizes2@hotmail.com>
2022-08-09 01:22:46 +0200

    create components.md, todoList and models

---
+++ src/components/ToDoList/ToDoList.tsx
@@ -0,0 +1,11 @@
+const ToDoList = () => {
+  return (
+    <ul className="tasks-list">
+      <li className="tasks-list__task">
+        Infiltrate fallen angel amongst humankind
+      </li>
+    </ul>
+  );
+};
+
+export default ToDoList;
--- src/models/models.ts
+++ src/models/models.ts
@@ -1,5 +1,5 @@
 export interface Task {
   id: number;
-  name: string;
+  task: string;
   done: boolean;
 }
