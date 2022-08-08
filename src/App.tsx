f907b9dfc34d9033b829bd12bfcc7087262a8660
Fernando Paulino <xizes2@hotmail.com>
2022-08-08 21:21:51 +0200

    initiate App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import ToDoList from "./components/ToDoList/ToDoList";
 
 function App() {
   return (
     <>
-      <h1>Things</h1>
+      <h1>To Do List from Hell 🔥 </h1>
+      <ToDoList />
     </>
   );
 }
